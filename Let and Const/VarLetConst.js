76352f73159a7cb940a22dcedbac880ea173f48f
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-08-30 20:31:41 +0300

    Let and Const

---
+++ Let and Const/VarLetConst.js
@@ -0,0 +1,24 @@
+// classic variable defining
+
+/* var myName = "zeus";
+console.log(myName);
+
+myName="Zeus";
+console.log(myName); */
+
+// let - changable
+
+/* let myName = "zeus";
+console.log(myName);
+
+myName="Zeus";
+console.log(myName); */
+
+// const - constant
+
+const myName = "zeus";
+console.log(myName);
+
+myName="Zeus";//can't change const
+console.log(myName);
+
