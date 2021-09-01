83466a164e618002d095b9044efe4c2fcbcae753
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-01 14:51:12 +0300

    Changes on data and coloring

--- 01-first-react-app/src/App.js
+++ 01-first-react-app/src/App.js
@@ -4,34 +4,34 @@ function App() {
     // array of objects
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      title: "Norsk Utdanning",
+      amount: 126000,
+      date: new Date(2022, 7, 15),
     },
     {
       id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
+      title: "Study Permit",
+      amount: 5000,
+      date: new Date(2022, 8, 15),
     },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
+      title: "Job Applications",
+      amount: "3 years of my life expectancy",
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: "Dinner",
+      amount: 100,
+      date: new Date(2021, 8, 1),
     },
   ];
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <h2>Expense List!</h2>
+      <p>Let's get started!</p>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
