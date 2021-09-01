ea4f147d3621df456012ed38767c88c2f8ab2f68
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-01 11:05:15 +0300

    Divs and a bit more complexity

--- 01-first-react-app/src/components/ExpenseItem.js
+++ 01-first-react-app/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>September 1st 2021</div>
+      <div>
+        <h2>Car Insuranca</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
