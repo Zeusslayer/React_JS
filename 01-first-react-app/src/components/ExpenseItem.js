b2b899610f1314938591c8632a03bf49cfe6449e
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-01 16:17:19 +0300

    New component for Dates

--- 01-first-react-app/src/components/ExpenseItem.js
+++ 01-first-react-app/src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css"; // must import css to the js file
 function ExpenseItem(props) {
   // use the const to hard-code data and use props to access App.js attributes
@@ -9,17 +10,10 @@ function ExpenseItem(props) {
   //return should only return one div, there may be others inside it
   //adding css to divs with className
   //you can add operations inside the brackets
-  const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
-  const month = props.date.toLocaleString("en-GB", { month: "long" });
-  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date2={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">NOK {props.amount}</div>
