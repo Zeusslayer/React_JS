ea86338c7d946a7b238a025de4bca84b092e058a
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-01 11:35:43 +0300

    Dynamic data usage and Commenting

--- 01-first-react-app/src/components/ExpenseItem.js
+++ 01-first-react-app/src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
-import "./ExpenseItem.css";
+import "./ExpenseItem.css"; // must import css to the js file
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 8, 28); //september
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  //return should only return one div, there may be others inside it
+  //adding css to divs with className
+  //you can add operations inside the brackets
   return (
     <div className="expense-item">
-      <div>September 1st 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
