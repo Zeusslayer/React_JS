9944e2d047d173f9c1b63c2632f5942a9e7c34d8
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-01 12:51:16 +0300

    Props and Reusability

--- 01-first-react-app/src/components/ExpenseItem.js
+++ 01-first-react-app/src/components/ExpenseItem.js
@@ -1,17 +1,20 @@
 import "./ExpenseItem.css"; // must import css to the js file
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 8, 28); //september
+function ExpenseItem(props) {
+  // use the const to hard-code data and use props to access App.js attributes
+
+  /* const expenseDate = new Date(2021, 8, 28); //september
   const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+  const expenseAmount = 294.67; */
+
   //return should only return one div, there may be others inside it
   //adding css to divs with className
   //you can add operations inside the brackets
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
