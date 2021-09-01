fb81ae904b1cab560e4688fe1c1a02b013633f5f
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-01 15:27:29 +0300

    Better dates

--- 01-first-react-app/src/components/ExpenseItem.js
+++ 01-first-react-app/src/components/ExpenseItem.js
@@ -9,9 +9,17 @@ function ExpenseItem(props) {
   //return should only return one div, there may be others inside it
   //adding css to divs with className
   //you can add operations inside the brackets
+  const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-GB", { month: "long" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">NOK {props.amount}</div>
