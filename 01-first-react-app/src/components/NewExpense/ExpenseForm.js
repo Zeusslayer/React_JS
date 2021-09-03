092a6a116907911e60824df238044334ce3e7f30
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-03 04:05:23 +0300

    Two way binding and clearing the input

--- 01-first-react-app/src/components/NewExpense/ExpenseForm.js
+++ 01-first-react-app/src/components/NewExpense/ExpenseForm.js
@@ -46,15 +46,20 @@ const ExpenseForm = (props) => {
   };
 
   const submitHandler = (event) => {
-    event.preventDefault();
+    //submit button handler
+    event.preventDefault(); //prevents the refresh on submit
 
     const expenseData = {
+      //object for us to use
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -62,13 +67,18 @@ const ExpenseForm = (props) => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             min="0.10"
             step="0.10"
             onChange={amountChangeHandler}
@@ -79,6 +89,7 @@ const ExpenseForm = (props) => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
