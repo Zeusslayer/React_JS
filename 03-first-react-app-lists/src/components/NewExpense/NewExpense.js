e64f52faff3ad150091efbf3cedd2e72ea4d662e
Zeusslayer <86309091+Zeusslayer@users.noreply.github.com>
2021-09-07 15:30:45 +0300

    Added a button to show and hide the new expense

--- 03-first-react-app-lists/src/components/NewExpense/NewExpense.js
+++ 03-first-react-app-lists/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData, //gets the data from child + adds an id
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   //onSaveExpenseData points to a function, it can be executed when it's called
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancelEditing={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
